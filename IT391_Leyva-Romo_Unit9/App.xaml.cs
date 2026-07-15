f5b11d3bdb1bf2a98c68037dc75581b8b9eaeb16
MonicaLRomo <monicaromo24@yahoo.com>
2026-07-15 00:07:23 -0500

    Add project files.

---
+++ IT391_Leyva-Romo_Unit9/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IT391_Leyva_Romo_Unit9
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ IT391_Leyva-Romo_Unit9/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace IT391_Leyva_Romo_Unit9
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            var names = typeof(MainPage).Assembly.GetManifestResourceNames();
+            foreach (var name in names)
+            {
+                Debug.WriteLine(name);
+            }
+        }
+             public void Button_Click(object sender, EventArgs e)
+        {
+            OutputLabel.Text = "Thank you for clicking me 😊";
+            return;
+
+        }
+        public void ButtonClick(object sender, EventArgs e)
+        {
+            imgLogo.Source = "img_2023.png";
+            return;
+
+        }
+        public void ThirdButtonClick(object sender, EventArgs e)
+        {
+            MessageLabel.Text = "Have a great week!😁";
+            return;
+        }
+
+
+    }
+}
+
